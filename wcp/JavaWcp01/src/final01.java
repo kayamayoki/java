14a7e8c2997ed4161b7dbb238f88161f920954ab
kayamayoki <k32142004@gmail.com>
2021-05-19 13:12:11 +0900

    条件分岐

---
+++ wcp/JavaWcp01/src/final01.java
@@ -0,0 +1,10 @@
+
+public class final01 {
+	  public static void main(String[] args){
+		    final float TAX = 0.1F;
+
+		    float price = 800 * (1 + TAX);
+		    System.out.println("料金は" + price + "です");
+		  }
+
+}
